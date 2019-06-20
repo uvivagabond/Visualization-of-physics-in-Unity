e6c2e2274fdf4cd1e1d218bbd9903c85b8da176c
Rafal Tadajewski <darthshopen@o2.pl>
2019-06-20 14:59:19 +0200

    Multiplication tests

--- Assets/Scripts/Test Scripts For Quaternions/VectorQuaternionMultiplication.cs
+++ Assets/Scripts/Test Scripts For Quaternions/VectorQuaternionMultiplication.cs
@@ -12,19 +12,6 @@ public class VectorQuaternionMultiplication : MonoBehaviour
   
     void Update()
     {
-        Quaternion rotation = Quaternion.Euler(eulerAngles);
-        end1 = rotation * Vector3.right;
-
-        // Prep for calculations
-        Quaternion positionQuaternion = new Quaternion(
-            1, 0, 0, 0);
-        Quaternion inverseRotation = Quaternion.Inverse(rotation);
-
-        // Calculate new position
-        Quaternion newPositionQuat = rotation * positionQuaternion * inverseRotation;
-
-        // Store result
-       end2 = new Vector3(
-            newPositionQuat.x, newPositionQuat.y, newPositionQuat.z);
+       
     }
 }
